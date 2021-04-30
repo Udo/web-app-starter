ee200da6d0f68a9e08d191bfad14174197d8c049
Udo Schroeter <udo@openfu.com>
2021-04-30 02:50:53 +0200

    added db-file

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 
 	include('lib/ulib.php');
+	include('lib/db-file.php');
 	include('config/settings.php');
 	include('lib/render.php');
-	
+
 	URL::MakeRoute();
 	User::init();
 
-	ob_start();	
+	ob_start();
 	print(component(first(URL::$route['l-path'], 'index'), array($prop['id'] => $_REQUEST['attr']['id'])));
 	$content = ob_get_clean();
-		
+
 	include(cfg('theme/path').'/page.'.URL::$route['content-type'].'.php');
-	
+
 	Log::audit('page:'.URL::$route['page'], URL::$route['l-path']);
